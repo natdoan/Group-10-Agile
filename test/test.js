11c5da2c12ddcc99e4ff3b5b62c0a9180f8ee7a6
Brody <vothbrody@gmail.com>
2019-05-03 09:33:05 -0700

    Add and edit post initial

--- test/test.js
+++ test/test.js
@@ -1,13 +1,10 @@
-const request = require('supertest');
 const assert = require('chai').assert;
 const expect = require('chai').expect;
 const chai = require('chai')
 const chaiHttp = require('chai-http');
-const should = require('should');
 
 const {DateTime} = require('luxon');
 const date = require('../forum').get_date;
-const forum = require('../forum')
 
 chai.use(chaiHttp);
 
@@ -65,19 +62,51 @@ describe('Invalid login', function () {
     })
 })
 
-// describe('Add post', function () {
-//     it('Should add post to database', function (done) {
-//         chai.request('http://localhost:8080')
-//             .post('/add_post')
-//             .type('form')
-//             .send({title: 'test title new', message: 'test message', username: 'man'})
-//             .then(function (res) {
-//                 var str = res.text;
-//                 console.log(str)
-//                 var page_text = /test title new/i;
-//                 var result = page_text.test(str);
-//                 assert.equal(result, true);
-//                 done()
-//             })
-//     })
-// })
+describe('Add post', function() {
+    it('Should add post to database', function(done) {
+        var agent = chai.request.agent('http://localhost:8080')
+        agent
+            .post('/login')
+            .type('form')
+            .send({username: 'tester', password: 'test'})
+            .then(function(res) {
+                return agent
+                    .post('/add_post')
+                    .type('form')
+                    .send({title: 'test title new', message: 'test message'})
+                    .then(function(res) {
+                        var str = res.text;
+                        var page_text = /test title new/i;
+                        var result = page_text.test(str);
+                        assert.equal(result, true);
+                        done()
+                    });
+            });
+        agent.close();
+    })
+})
+
+describe('Edit post', function() {
+    it('Should edit a post in the database', function(done) {
+        var agent = chai.request.agent('http://localhost:8080')
+        var thread = chai.request.agent('http://localhost:8080/thread/5ccc5d18e7c87927c4b668ec')
+        agent
+            .post('/login')
+            .type('form')
+            .send({username: 'tester', password: 'test'})
+            .then(function(res) {
+                return thread
+                    .post('/edit_post')
+                    .type('form')
+                    .send({reply_id: '5ccc5d18e7c87927c4b668ec', reply_username: 'tester', edit_reply_textarea: "message"})
+                    .then(function(res) {
+                        var str = res.text;
+                        var page_text = /edit successful/i;
+                        var result = page_text.test(str);
+                        assert.equal(result, true);
+                        done()
+                    });
+            });
+        agent.close();
+    })
+})
